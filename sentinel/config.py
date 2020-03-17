c1aa3fdbba3f12381308ce42f5da473d844c382a
icemilokepal <48389622+icemilokepal@users.noreply.github.com>
2020-03-18 00:09:58 +0700

    Update config.py

--- sentinel/config.py
+++ sentinel/config.py
@@ -3,7 +3,7 @@ from akad.ttypes import ApplicationType
 import re
 
 class Config(object):
-    LINE_HOST_DOMAIN            = 'https://gd2.line.naver.jp'
+    LINE_HOST_DOMAIN            = 'https://gw.line.naver.jp'
     LINE_OBS_DOMAIN             = 'https://obs-sg.line-apps.com'
     LINE_TIMELINE_API           = 'https://gd2.line.naver.jp/mh/api'
     LINE_TIMELINE_MH            = 'https://gd2.line.naver.jp/mh'
@@ -18,42 +18,23 @@ class Config(object):
     LINE_CHAN_QUERY_PATH        = '/CH4'
     LINE_SQUARE_QUERY_PATH      = '/SQS1'
     LINE_SHOP_QUERY_PATH        = '/SHOP4'
+    LINE_LIFF_QUERY_PATH        = '/LIFF1'
 
     CHANNEL_ID = {
         'LINE_TIMELINE': '1341209850',
         'LINE_WEBTOON': '1401600689',
         'LINE_TODAY': '1518712866',
-        'LINE_STORE': '1376922440',
-        'LINE_MUSIC': '1381425814',
-        'LINE_SERVICES': '1459630796'
+        'LINE_STORE': '1376922440'
     }
 
-    APP_VERSION = {
-        'ANDROID': '8.14.2',
-        'IOS': '8.14.2',
-        'ANDROIDLITE': '2.1.0',
-        'BIZANDROID': '1.7.2',
-        'BIZIOS': '1.7.5',
-        'BIZWEB': '1.0.22',
-        'DESKTOPWIN': '5.9.0',
-        'DESKTOPMAC': '5.9.0',
-        'IOSIPAD': '8.14.2',
-        'CHROMEOS': '2.1.5',
-        'WIN10': '5.5.5',
-        'DEFAULT': '8.11.0'
-    }
-
-    APP_TYPE    = 'WIN10'
-    APP_VER     = APP_VERSION[APP_TYPE] if APP_TYPE in APP_VERSION else APP_VERSION['DEFAULT']
-    CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'Uwewww'
-    SYSTEM_VER  = '10.0.14'
-    IP_ADDR     = '8.8.8.8'
+    APP_TYPE = ApplicationType._VALUES_TO_NAMES[368]
+    APP_VER = "2.1.5"
+    CARRIER = '51089, 1-0'
+    SYSTEM_NAME = 'ELFOX-BOT'
+    SYSTEM_VER = '11.2.5'
+    IP_ADDR = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
-    def __init__(self, appType=None):
-        if appType:
-            self.APP_TYPE = appType
-            self.APP_VER = self.APP_VERSION[self.APP_TYPE] if self.APP_TYPE in self.APP_VERSION else self.APP_VERSION['DEFAULT']
-        self.APP_NAME = '%s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)
-        self.USER_AGENT = 'Line/%s' % self.APP_VER
+    def __init__(self):
+        self.APP_NAME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'
+        self.USER_AGENT = 'CHROMEOS\t2.3.2\tChrome OS\t1'
